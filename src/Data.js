2d10a899f6cd10e2721f41f013160b54b3b5a106
vishakhaajani <vishakhaajani28@gmail.com>
2024-05-13 17:16:56 +0530

    project

---
+++ src/Data.js
@@ -0,0 +1,96 @@
+let category = [
+    {
+      id : 1,
+      name : "Brackfast",
+      reciep : ""
+    },
+    {
+      id : 2,
+      name : "Lunch"
+    },
+    {
+      id : 3,
+      name : "Dinner"
+    },
+    {
+      id : 4,
+      name : "Snacks"
+    }
+  ]
+
+  let item = [
+    {
+      id : 1,
+      img : "https://modinatheme.com/foodking/wp-content/uploads/2024/04/product-12.png",
+      name : "PASTA",
+      category : "Snacks"
+    },
+    {
+      id : 2,
+      img : "https://modinatheme.com/foodking/wp-content/uploads/2024/03/french-fry.png",
+      name : "FRENCH FREIES",
+      category : "Snacks"
+    },
+    {
+      id : 3,
+      img : "https://modinatheme.com/foodking/wp-content/uploads/2024/03/pizza-2.png",
+      name : "PIZZA",
+      category : "Dinner"
+    },
+    {
+      id : 4,
+      img : "https://modinatheme.com/foodking/wp-content/uploads/2024/03/burger-2.png",
+      name : "BURGER",
+      category : "Dinner"
+    },
+    {
+      id : 5,
+      img : "https://modinatheme.com/foodking/wp-content/uploads/2024/03/beef-ruti.png",
+      name : "ROTI SUBJI",
+      category : "Lunch"
+    },
+    {
+      id : 6,
+      img : "https://demo2.wpopal.com/fazfood/wp-content/uploads/2023/10/product_14-600x600.png",
+      name : "LOADED FRIES",
+      category : "Brackfast"
+    },
+    {
+      id : 7,
+      img : "https://themeholy.com/wordpress/pizzan/wp-content/uploads/2023/06/Image-6-1-180x180.png",
+      name : "BIRYANI",
+      category : "Lunch"
+    },
+    {
+      id : 8,
+      img : "https://themeholy.com/wordpress/pizzan/wp-content/uploads/2023/06/Image-7-180x180.png",
+      name : "NOODLES",
+      category : "Snacks"
+    },
+    {
+      id : 9,
+      img : "https://themeholy.com/wordpress/pizzan/wp-content/uploads/2022/12/image-31.png",
+      name : "ALOO PARATHA",
+      category : "Brackfast"
+    },
+    {
+      id : 10,
+      img : "https://themeholy.com/wordpress/pizzan/wp-content/uploads/2023/03/product_coffee_6.png",
+      name : "ORGANICE TEA",
+      category : "Brackfast"
+    },
+    {
+      id : 11,
+      img : "https://themeholy.com/wordpress/pizzan/wp-content/uploads/2023/06/Image-8-180x180.png",
+      name : "MAGII",
+      category : "Dinner"
+    },
+    {
+      id : 12,
+      img : "https://themeholy.com/wordpress/pizzan/wp-content/uploads/2023/06/Image-5-180x180.png",
+      name : "CHICKEN",
+      category : "Lunch"
+    }
+  ]
+
+  export {category,item}
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React , {useState} from 'react'
 import './menu.css'
 
-const Menu = ({ category, item, filterRecord }) => {
+const Menu = ({ category, item}) => {
 
-    return (
+    const [filteredCategory, setFilteredCategory] = useState(category)
+    const [filteredItem, setFilteredItem] = useState(item)
+
+    const filterRecord = (cat) => {
+        if(cat == "all"){
+            setFilteredItem(item);
+        }
+        else{
+          let filterItem = item.filter((menu) => menu.category === cat)
+          setFilteredItem(filterItem);
+        } 
+    }
+
+    return ( 
         <div align="center">
             <button onClick={() => filterRecord("all")} className='py-2 px-4 mb-5 mt-3  text-light mx-3'>All</button>
             {
-                category.map((val)=>{
+                filteredCategory.map((val)=>{
                     return(
                         <button onClick={() => filterRecord(val.name)} key={val.id} className='m-3 py-2 px-4  text-light'>{val.name}</button>  
                     )
@@ -15,9 +28,9 @@ const Menu = ({ category, item, filterRecord }) => {
             }
             <div className="row">
             {
-                item.map((val)=>{
+                filteredItem.map((val)=>{
                     return(
-                            <div className="col-lg-3">
+                            <div className="col-lg-3" key={val.id}>
                             <div className="card" style={{width: '18rem'}}>
                                 <div className="img">
                                 <img src={val.img} className="card-img-top" alt="..." width={244} height={244} style={{objectFit:'cover'}}/>
